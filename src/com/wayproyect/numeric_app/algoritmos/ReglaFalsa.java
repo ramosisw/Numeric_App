fa6828ef78079ae56c17270ac33c3d05fab445e8
Julio C. Ramos <ramos.isw@gmail.com>
2013-11-14 23:47:38 -0700

    Rejected commit: Default

---
+++ src/com/wayproyect/numeric_app/algoritmos/ReglaFalsa.java
@@ -0,0 +1,69 @@
+package com.wayproyect.numeric_app.algoritmos;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ADMIN
+ * Date: 14/11/13
+ * Time: 11:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReglaFalsa {
+    private String function;
+
+    /**
+     * Este metodo crea un funcion a la cual se le aplicara el método de
+     * regla falsa teniendo como parametro de entrada un double x, el cual
+     * sirve para la construccion de la funcion dentro del metodo
+     *
+     * @param x
+     * @return
+     */
+    private double funcion(double x) {
+        try {
+            return new EvalFunction().eval(function, x);
+        } catch (Exception e) {
+            return Double.NaN;
+        }
+    }
+
+    /**
+     * Metodo de regla falsa
+     *
+     * @param a
+     * @param b
+     * @param error
+     * @return
+     */
+    public double metodoDeReglaFalsa(String function, double a, double b, double error) {
+        this.function = function;
+        double fa, fb, fc;
+        double c = 0;
+        if ((funcion(a) * funcion(b) > 0)) {
+            System.out.println("Error en el intervalo inicial, para este intervalo" +
+                    " no existen raices. ");
+        } else {
+            fa = funcion(a);
+            fb = funcion(b);
+            c = b - ((fb * (b - a)) / (fb - fa));
+            fc = funcion(c);
+            do {
+                if ((fa * fc) > 0) {
+                    a = c;
+                    fa = funcion(a);
+                    fb = funcion(b);
+                    c = b - ((fb * (b - a)) / (fb - fa));
+                    fc = funcion(c);
+                } else if ((fb * fc) > 0) {
+                    b = c;
+                    fa = funcion(a);
+                    fb = funcion(b);
+                    c = b - ((fb * (b - a)) / (fb - fa));
+                    fc = funcion(c);
+
+                }
+            } while (Math.abs(fc) * 100 > error);
+
+        }
+        return c;
+    }
+}
--- src/com/wayproyect/numeric_app/vistas/regla_falsa/ReglaFalsa_Manual.java
+++ src/com/wayproyect/numeric_app/vistas/regla_falsa/ReglaFalsa_Manual.java
@@ -2,6 +2,8 @@ package com.wayproyect.numeric_app.vistas.regla_falsa;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 import com.wayproyect.numeric_app.R;
 
 /**
@@ -12,8 +14,23 @@ import com.wayproyect.numeric_app.R;
  * To change this template use File | Settings | File Templates.
  */
 public class ReglaFalsa_Manual extends Activity {
+    private EditText function;
+    private EditText a;
+    private EditText b;
+    private EditText er;
+    private TextView txt_xr_r;
+    private TextView txt_er_r;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.regla_falsa_manual);
+        init();
+    }
+
+    private void init() {
+        this.function = (EditText) findViewById(R.id.txt_fx);
+        this.a = (EditText) findViewById(R.id.txt_a);
+        this.b = (EditText) findViewById(R.id.txt_b);
+        this.er = (EditText) findViewById(R.id.txt_er);
     }
 }
